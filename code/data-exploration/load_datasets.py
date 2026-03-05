8b147defbb0c87fde25b6ebb99b43e66e39cef33
zekemackay06 <zekemackay06@gmail.com>
2026-03-05 09:31:35 -0600

    Update with new datasets, add Dataset child class for datasets

--- code/data-exploration/load_datasets.py
+++ code/data-exploration/load_datasets.py
@@ -1,35 +1,57 @@
 from huggingface_hub import login
-from os import getcwd, mkdir
-from os.path import exists, join
 import pandas as pd
+from torch.utils.data import Dataset
 
-def load_datasets(path = join(getcwd(),'data'), exclude_datasets = []):
-    """Load training datasets - AdvBench, HelpSteer, TruthfulQA, Alpaca.
-    
-    Optional args:
-        path (path-like object): Path to download dataset CSV files. Defaults to ./data/
-        exclude_datasets (container of str): dataset names to exclude (adv_bench, help_steer, truthful_qa, alpaca)
-    """
-    login()
+hf_alpaca_path = "hf://datasets/tatsu-lab/alpaca/data/train-00000-of-00001-a09b74b3ef9c3b56.parquet"
+hf_beaver_tails_path = "hf://datasets/PKU-Alignment/BeaverTails/round0/330k/train.jsonl.xz"
 
-    if not exists(path):
-        mkdir(path)
+def alpaca_df(path=hf_alpaca_path):
+    """Load Alpaca from HuggingFace or local path to Pandas DF"""
+    assert path.endswith(".parquet"), "Alpaca path must be a .parquet file"
+    return pd.read_parquet(path)
 
+def beaver_tails_df(path=hf_beaver_tails_path):
+    """Load BeaverTails from HuggingFace or local path to Pandas DF"""
+    if path.endswith('.json1.xz'):
+        return pd.read_json(path)
+    elif path.endswith('.parquet'):
+        return pd.read_parquet(path)
+    raise Exception("BeaverTails path must be a .parquet file")
 
-    if 'adv_bench' not in exclude_datasets:
-        adv_bench = pd.read_parquet("hf://datasets/walledai/AdvBench/data/train-00000-of-00001.parquet")
-        adv_bench.to_csv(join(path,'adv_bench.csv'))
-    if 'help_steer' not in exclude_datasets:
-        help_steer = pd.read_json("hf://datasets/nvidia/HelpSteer/train.jsonl.gz", lines=True)
-        help_steer.to_csv(join(path,'help_steer.csv'))
-    if 'truthful_qa' not in exclude_datasets:
-        truthful_qa = pd.read_csv("hf://datasets/domenicrosati/TruthfulQA/train.csv")
-        truthful_qa.to_csv(join(path,'truthful_qa.csv'))
-    if 'alpaca' not in exclude_datasets:
-        alpaca = pd.read_parquet("hf://datasets/tatsu-lab/alpaca/data/train-00000-of-00001-a09b74b3ef9c3b56.parquet")
-        alpaca.to_csv(join(path,'alpaca.csv'))
+def load_alpaca_local(in_path=hf_alpaca_path,out_path='./data/alpaca.parquet'):
+    """Download Alpaca"""
+    alpaca_df = alpaca_df(in_path)
+    alpaca_df.to_parquet(path=out_path)
 
+def load_beaver_tails_local(in_path=hf_beaver_tails_path,out_path='./data/beaver_tails.parquet'):
+    "Download BeaverTails"
+    beaver_tails_df = beaver_tails_df(in_path)
+    beaver_tails_df.to_parquet(path=out_path)
 
+class HarmDataset(Dataset):
+    def __init__(self,dataset,path=hf_alpaca_path):
+        assert dataset in {'alpaca','beaver_tails'}, "dataset must be either alpaca or beaver_tails"
+        self.dataset = dataset
+        if dataset == 'alpaca':
+            self.df = alpaca_df(path)
+        if dataset == 'beaver_tails':
+            self.df = beaver_tails_df(path)
+            self.beaver_tails_prune_unsafe()
+    def __len__(self):
+        return len(self.df)
+    def __getitem__(self, idx):
+        row = self.df.iloc[idx]
+        if self.dataset == 'alpaca':
+            instr = row[['instruction','input']]
+            out = row['output']
+        if self.dataset == 'beaver_tails':
+            instr = row[['prompt','category']]
+            out = row['response']
+        return instr,out
+    def beaver_tails_prune_unsafe(self):
+        assert self.dataset == 'beaver_tails'
+        self.df = self.df[not self.df['is_safe']]
+        
 if __name__ == "__main__":
-    load_datasets()
-
+    load_alpaca_local()
+    load_beaver_tails_local()
--- code/data-exploration/readme.md
+++ code/data-exploration/readme.md
@@ -1,4 +1,4 @@
 # Exploratory Data Analysis
 
 ## Load Datasets
-This file will load the four datasets into ./data. When you run it, it will ask for a huggingface token. You can create huggingface tokens [at this link](https://huggingface.co/settings/tokens). The **Repositories --> Read Read access to contents of all public gated repos you can access** setting must be checked for the token to function, and your account must have requested access to the datasets.
+This file will load the two datasets into ./data, as well as creating a custom Torch Dataset class to load them. When you run it, it will ask for a huggingface token. You can create huggingface tokens [at this link](https://huggingface.co/settings/tokens). The **Repositories --> Read Read access to contents of all public gated repos you can access** setting must be checked for the token to function.S
