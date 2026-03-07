298fea7486cbaae52df9795545477f2a1ba0f78a
Evan Scamehorn <scamehorn@wisc.edu>
2026-03-07 15:17:03 -0600

    fix deprecated param name

--- code/actsvd/main_low_rank_diff.py
+++ code/actsvd/main_low_rank_diff.py
@@ -334,7 +334,7 @@ if __name__ == "__main__":
     def get_llm(model_name, cache_dir="llm_weights"):
         model = AutoModelForCausalLM.from_pretrained(
             modeltype2path[model_name],
-            torch_dtype=torch.bfloat16,
+            dtype=torch.bfloat16,
             cache_dir=cache_dir,
             low_cpu_mem_usage=True,
             device_map="cuda",
